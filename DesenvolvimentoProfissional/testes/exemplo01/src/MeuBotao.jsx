804ed3b08516c26ef4a8bf0ce142afae5f4529f9
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2025-06-09 21:24:33 -0300

    Changes to be committed: 	modified:   DesenvolvimentoProfissional/testes/README.md 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/.gitignore 	modified:   DesenvolvimentoProfissional/testes/exemplo01/README.md 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/eslint.config.js 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/index.html 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/package-lock.json 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/package.json 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/public/vite.svg 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/App.css 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/App.jsx 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/MeuBotao.control.test.jsx 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/MeuBotao.integration.test.jsx 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/MeuBotao.jsx 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/MeuBotao.test.jsx 	modified:   DesenvolvimentoProfissional/testes/exemplo01/src/MeuBotao.unit.test.jsx 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/assets/react.svg 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/index.css 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/index.js 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/src/main.jsx 	deleted:    DesenvolvimentoProfissional/testes/exemplo01/vite.config.js

--- DesenvolvimentoProfissional/testes/exemplo01/src/MeuBotao.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MeuBotao({ onClick, children }) {
-  return (
-    <button onClick={onClick} data-testid="meu-botao">
-      {children}
-    </button>
-  );
-}
--- DesenvolvimentoProfissional/testes/exemplo01/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MeuBotao from "./MeuBotao";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <MeuBotao onClick={() => alert("Botão clicado!")}>Clique aqui</MeuBotao>
-);
